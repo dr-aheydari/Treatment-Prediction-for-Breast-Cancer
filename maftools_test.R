26955dbc02b845c25603f3d156a692861ff95ccf
dr-aheydari <aliheydari@ucdavis.edu>
2019-02-22 14:56:58 -0800

    taking everything away

--- maftools_test.R
+++
@@ -1,8 +0,0 @@
-# this is an experiment with the maftools
-
-require(maftools)
-
-#laml.maf = system.file('extdata', 'tcga_laml.maf.gz', package = 'maftools') #path to TCGA LAML MAF file
-#laml.clin = system.file('extdata', 'tcga_laml_annot.tsv', package = 'maftools') # clinical information containing survival information and histology. This is optional
-
-laml = read.maf(maf = laml.maf, clinicalData = laml.clin)
